d67c52268574ab3ec7d1097aabc01ee3daf6e47d
bowowputra31 <triwibowoputra49@gmail.com>
2021-06-17 23:42:57 +0900

    strapidb

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '903197e36b0de880dde06101b2ee4186'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '273c4265-88d1-4b1c-95a6-11487dfa5476'
+};
